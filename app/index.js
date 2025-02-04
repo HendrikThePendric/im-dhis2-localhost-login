6b8180c714547d021cd23ace198e023f0709585e
HendrikThePendric <hendrik@dhis2.org>
2025-02-04 10:45:19 +0100

    chore: improve logging and reject responses when not OK

--- app/index.js
+++ app/index.js
@@ -13,26 +13,33 @@ window.login = async function () {
 };
 
 async function getIsNewLoginApiAvailable(server) {
+  const path = "api/loginConfig";
   try {
     // if loginConfig is available, the instance can use new endpoints
-    await fetch(`${server}/api/loginConfig`, {
+    await fetch(`${server}/${path}`, {
       headers: {
         "X-Requested-With": "XMLHttpRequest",
         Accept: "application/json",
       },
-    });
-    console.log("USE NEW ENDPOINT");
+    }).then(rejectIfNotOK);
+
+    console.log(
+      `GET ${path} returned an OK response => use NEW login endpoint`,
+    );
     return true;
   } catch (e) {
     // if loginConfig is not available, the instance must use old endpoints
-    console.log("USE OLD ENPOINT");
+    console.log(
+      `GET ${path} returned an ERROR response => use OLD login endpoint`,
+    );
     return false;
   }
 }
 
 async function loginWithNewEndpoint(server) {
+  const path = "api/auth/login";
   try {
-    await fetch(`${server}/api/auth/login`, {
+    await fetch(`${server}/${path}`, {
       body: JSON.stringify({
         username,
         password,
@@ -43,15 +50,20 @@ async function loginWithNewEndpoint(server) {
         Accept: "application/json",
         "Content-Type": "application/json",
       },
-    });
+    }).then(rejectIfNotOK);
+    console.log(`POST to ${path} returned an OK response => NEW login SUCCESS`);
   } catch (e) {
-    console.log("LOGIN ERROR - NEW: ", e);
+    console.log(
+      `POST to ${path} returned an ERROR response => NEW login FAILURE`,
+    );
+    throw e;
   }
 }
 
 async function loginWithOldEndpoint(server) {
+  const path = "dhis-web-commons-security/login.action";
   try {
-    await fetch(`${server}/dhis-web-commons-security/login.action`, {
+    await fetch(`${server}/${path}`, {
       body: `j_username=${encodeURIComponent(
         username,
       )}&j_password=${encodeURIComponent(password)}`,
@@ -61,23 +73,43 @@ async function loginWithOldEndpoint(server) {
         Accept: "application/json",
         "Content-Type": "application/x-www-form-urlencoded",
       },
-    });
+    }).then(rejectIfNotOK);
+    console.log(`POST to ${path} returned an OK response => OLD login SUCCESS`);
   } catch (e) {
-    console.error("LOGIN ERROR - OLD: ", e);
+    console.log(
+      `POST to ${path} returned an ERROR response => OLD login ERROR`,
+    );
+    throw e;
   }
 }
 
 async function getInfoFromAuthenticatedResource(server) {
+  const path = "api/system/info";
   try {
-    const data = await fetch(`${server}/api/system/info`, {
+    const data = await fetch(`${server}/${path}`, {
       credentials: "include",
       headers: {
         "X-Requested-With": "XMLHttpRequest",
         Accept: "application/json",
       },
-    }).then((resp) => resp.json());
-    console.log("AUTHENTICATED REQUEST SUCCESS", data);
+    })
+      .then(rejectIfNotOK)
+      .then((resp) => resp.json());
+    console.log(
+      `Authenticated GET request to ${path} returned an OK response => data:\n`,
+      data,
+    );
   } catch (e) {
-    console.error("AUTHENTICATED REQUEST FAILURE", e);
+    console.log(
+      `Authenticated GET request to ${path} returned an ERROR response`,
+    );
+    throw e;
   }
+}
+
+function rejectIfNotOK(response) {
+  if (!response.ok) {
+    return Promise.reject(response);
+  }
+  return Promise.resolve(response);
 }
